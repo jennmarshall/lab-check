3a090a6874cd99332b08a75d9129b4a4afa3946d
Jenn <163761956+jennmarshall@users.noreply.github.com>
2026-07-24 00:07:26 +0100

    - increasing reusability of codf, by isolating database functions in a separate file. Then calling them from outside

---
+++ src/db.py
@@ -0,0 +1,32 @@
+import sqlite3 as sql
+
+def connect():
+    try:
+        db = sql.connect('database.db')
+        print(f"INFO: Opened SQLite database with version {sql.sqlite_version} successfully.")
+        return db
+    except sql.OperationalError as e:
+        print(f"ERROR: Failed to open database: {e}")
+
+def disconnect(db):
+    try:
+        db.close()
+        print(f"INFO: Database connection closed successfully.")
+    except sql.OperationalError as e:
+        print(f"ERROR: Failed to close database: {e}")
+
+def execute(db, statement):
+    try:
+        cs = db.cursor()
+        cs.execute(statement)
+        db.commit()
+        print(f"INFO: SQL statement executed successfully.")
+    except sql.OperationalError as e:
+        print(f"ERROR: Failed to execute SQL statement: {e}")
+
+def build():
+    db = connect()
+    for statement in open('src/builders/build-db.sql').read().split(';'):
+        if statement.strip():
+            execute(db, statement)
+    disconnect(db)
